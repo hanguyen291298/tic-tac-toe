ebf27bb34717c1cd899fab7c164fc45bd2c08b00
hanguyen291298 <thuyha291298@gmail.com>
2023-10-02 17:11:35 +0700

    rebuild the boardgame

--- index.js
+++ index.js
@@ -1,43 +1,60 @@
 
 const gameBoard = (()=>{
-    
-    const create_grid = function(s){
-        const size = document.querySelector(".square-size")
-        for (let i = 0; i < s; i++){
-            let row = document.createElement("div")
-            row.classList.add(`row-${i}`)
-            for (let j = 0; j < s; j++){
-                const square = document.createElement("div")
-                square.classList.add("square")
-                row.appendChild(square)
-            }
-            size.appendChild(row)
-            
+    let boardgame = ['', '', '', '', '', '', '', '', '']
+    const size = document.querySelector(".board")
+
+    const create_grid = function(){
+        for (let i = 0; i < boardgame.length; i++){
+            let cell = document.createElement("div")
+            cell.classList.add("cell")
+            size.appendChild(cell)            
         }
     }
-    return {create_grid}
+
+    const restart = function(){
+        const restart_btn = document.querySelector(".restart")
+        restart_btn.addEventListener("click", ()=>{
+            size.textContent = "";
+            gameBoard.create_grid()
+        })
+    }
+    return {create_grid, restart}
 })()
     
-gameBoard.create_grid(3)
+gameBoard.create_grid()
+gameBoard.restart()
+
+
 
 const displayController = (()=>{
-    let currentPlayer = "";  
+    const size = document.querySelector(".board")
+    let currentPlayer_1 = "";
+    let currentPlayer_2 = "";
     const mark_sign = ()=>{
         document.addEventListener("click", (event)=>{
             const mark = event.target.getAttribute("mark")
-           if( mark === "X" || mark === "O"){
-                currentPlayer = mark
+           if( mark === "X" ){
+                
+                currentPlayer_1 = "X";
+                currentPlayer_2 = "O";
+           }
+           else if (mark === "O"){
+                currentPlayer_1 = "O";
+                currentPlayer_2 = "X";
            }
         })
     }
 
+
     const show_mark = ()=>{
+       
         document.addEventListener("click", (event)=>{
             let square = event.target
-            console.log(square)
-            if(square.classList.contains("square") && square.textContent === ""){
-                
-                square.innerHTML = currentPlayer
+            let random_positon = Math.floor(Math.random() * 3)
+            const square_childs = size.querySelectorAll(".cell")
+            console.log(square_childs.length)
+            if(square.classList.contains("cell") && square.textContent === ""){
+                square.innerHTML = currentPlayer_1
                 
             }
         })
@@ -45,4 +62,11 @@ const displayController = (()=>{
     return {mark_sign, show_mark}   
 })()
 
+displayController.mark_sign()
 displayController.show_mark()
+
+
+
+const player = ()=>{
+
+}
